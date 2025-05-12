c3fab0580319bc19f0b475ceb1944cc7644e39fa
Ahsaan S. <misterabdulhaadi@gmail.com>
2025-05-12 08:24:13 +0500

    Create generated_config.cmake

---
+++ frontend /windows /flutter /ephemeral /generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\PMYLP\\flutter_windows_3.29.3-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\PMYLP\\Medical Assistant Model\\medical_llm_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\PMYLP\\flutter_windows_3.29.3-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\PMYLP\\Medical Assistant Model\\medical_llm_app"
+  "FLUTTER_ROOT=C:\\Users\\PMYLP\\flutter_windows_3.29.3-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\PMYLP\\Medical Assistant Model\\medical_llm_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\PMYLP\\Medical Assistant Model\\medical_llm_app"
+  "FLUTTER_TARGET=C:\\Users\\PMYLP\\Medical Assistant Model\\medical_llm_app\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\PMYLP\\Medical Assistant Model\\medical_llm_app\\.dart_tool\\package_config.json"
+)
